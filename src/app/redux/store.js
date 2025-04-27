347dec8fd35dae1265cd8967b8aaecdc049a9fd4
SarahHanks04 <sarahhanks04@gmail.com>
2025-04-27 20:38:16 +0100

    Refactor store.js for consistent formatting and remove unnecessary line breaks

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,15 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/authSlice';
-import userReducer from './slices/userSlice';
-import userActionsReducer from './slices/userActionsSlice'; 
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice";
+import userReducer from "./slices/userSlice";
+import userActionsReducer from "./slices/userActionsSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     users: userReducer,
-    userActions: userActionsReducer, 
+    userActions: userActionsReducer,
   },
 });
-
-
-
